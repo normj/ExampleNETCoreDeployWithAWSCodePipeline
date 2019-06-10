f364a64f1089a5cfcac9421a0059f27022265301
Norm Johanson <normj@amazon.com>
2019-06-10 02:48:15 -0700

    Test a change

--- ExampleNETCoreDeployWithAWSCodePipeline/Startup.cs
+++ ExampleNETCoreDeployWithAWSCodePipeline/Startup.cs
@@ -27,7 +27,7 @@ namespace ExampleNETCoreAWSBeanstalkCICD
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Testing deployment through AWS CodePipeline!");
+                await context.Response.WriteAsync("Testing deployment through AWS CodePipeline!!");
             });
         }
     }
